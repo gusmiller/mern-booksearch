47a5a150fc39371f43502d8d23fa77bc31bce1a6
Gus Miller <gustavo.miller@miller-hs.com>
2024-01-18 05:36:55 -0500

    remove typedefs - chase error

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -20,42 +20,6 @@ const typeDefs = `
      image: String
      link: String
   }
-
-  type Auth {
-     token: ID!
-     user: User
-  }
-
-  type User {
-     _id: ID
-     username: String
-     email: String
-     bookCount: Int
-     savedBooks: [Book]
-  }
-
-  # Define which queries the front end is allowed to make and what data is returned
-  type Query {
-     me: User
-     searchGoogleBooks(searchInput: String!): [Book]
-  }
-
-  type Mutation{
-     login(email: String!, password: String!): Auth
-     addUser(username: String!, email: String!, password: String!): Auth
-     saveBook(bookData: BookInput!): User
-     removeBook(bookId: ID!): User
-     )
-  }
-
-  input BookInput {
-     authors: [String]
-     description: String
-     title: String
-     bookId: ID
-     image: String
-     link: String
-   }
 `;
 
 module.exports = typeDefs;
