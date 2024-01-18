6510a2b54569c6f71098a19204903be07ca10818
Gus Miller <gustavo.miller@miller-hs.com>
2024-01-17 21:20:45 -0500

    Syntax error while compiling

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -13,7 +13,6 @@
 const { AuthenticationError } = require('apollo-server-express');
 const { User, Book } = require('../models');
 const { signToken } = require('../utils/auth');
-const { signToken, AuthenticationError } = require('../utils/auth');
 
 const resolvers = {
 
